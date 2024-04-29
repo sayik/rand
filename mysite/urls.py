4f8bd939aed42e470641affebbc5d8e7ee8deff9
sayi <sayikrishnapk@gmail.com>
2024-04-29 13:49:55 +0530

    Learnt urls clearly

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # this function post_detail itself is passed so that post/<int:pk> is passed
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('kappa/<str:who>/', views.who_made, name='who'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,5 +7,16 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('-id')
+    posts = Post.objects.filter(
+        created_date__lte=timezone.now()).order_by('-id')
     return render(request, 'mysite/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'mysite/post_detail.html', {'post': post})
+
+
+def who_made(request, who):
+    title = Post.objects.get(title=who)
+    return render(request, 'mysite/post_who.html', {'title': title})
